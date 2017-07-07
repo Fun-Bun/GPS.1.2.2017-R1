e6057d924e3ae3d901fbaf70dc0d73d00288686b
FattyMieo <fong2009@live.com.my>
2017-07-07 15:09:37 +0800

    Finished MainMenu and GameScene

---
+++ Assets/Scripts/Managers/MainMenuManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManagerScript : MonoBehaviour
+{
+	public string startGameScene;
+	public GameObject[] menuWindows;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(startGameScene);
+	}
+
+	public void OpenMenu(int menu)
+	{
+		menuWindows[menu].SetActive(true);
+	}
+
+	public void CloseMenu(int menu)
+	{
+		menuWindows[menu].SetActive(false);
+	}
+
+	public void SetupBGM(GameObject slider){}
+
+	public void SetupSFX(GameObject slider){}
+
+	public void ChangeBGM(GameObject slider){}
+
+	public void ChangeSFX(GameObject slider){}
+}
