f226961d7d7016cf27c05a938d5e5a4eebbf5ba3
Muhamad Luqman Shamsuddin <luqsam1@gmail.com>
2017-08-09 18:49:47 +0800

    More Sounds

---
+++ Assets/Scripts/Audio/Player_SoundEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_SoundEffects : MonoBehaviour {
+	//! Sound Effects from Animation Event.
+
+	public void jumpingSound()
+	{
+		SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_PL_JUMPING);
+	}
+
+	public void walkingSound()
+	{
+		SoundManagerScript.Instance.PlaySFX(AudioClipID.SFX_PL_WALKING);
+	}
+}
